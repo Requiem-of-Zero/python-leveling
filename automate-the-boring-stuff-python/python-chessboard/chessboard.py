2b11919b4a48de67336f7a257ef9a84cc69eede1
requiem-of-zero <theesamwong@gmail.com>
2025-07-19 22:13:19 -0700

    fix work on wrong file

--- automate-the-boring-stuff-python/python-chessboard/chessboard.py
+++ automate-the-boring-stuff-python/python-chessboard/chessboard.py
@@ -3,7 +3,7 @@ import sys, copy
 STARTING_PIECES = {'a8': 'bR', 'b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
 'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR', 'a7': 'bP', 'b7': 'bP',
 'c7': 'bP', 'd7': 'bP', 'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
-'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ', 'e1': 'wK', 'f1': 'wB',
+'a1': 'wR', 'b1': 'wN', 'c1': 'ww', 'd1': 'wQ', 'e1': 'wK', 'f1': 'ww',
 'g1': 'wN', 'h1': 'wR', 'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
 'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'}
 
@@ -38,49 +38,65 @@ BOARD_TEMPLATE = """
 WHITE_SQUARE = '||'
 BLACK_SQUARE = '  '
 
-def print_chessboard(board):
-  squares = []
-  is_white_square = True
-  for y in '87654321':
-    for x in 'abcdefgh':
-      print(x,y, is_white_square) # DEBUG: Show coordinates
 
-      if x+y in board.keys():
-        squares.append(board[x+y])
-      else:
-        if is_white_square:
-          squares.append(WHITE_SQUARE)
-        else:
-          squares.append(BLACK_SQUARE)
-      is_white_square = not is_white_square
-    is_white_square = not is_white_square
-  print(BOARD_TEMPLATE.format(*squares)) # * before squares tells Python to pass the values in the list as individual arguments
+def print_chess_board(board):
+    squares = []
+    is_white_square = True
+    for y in '87654321':
+        for x in 'abcdefgh':
+            #print(x, y, is_white_square)  # DEBUG: Shows coordinates in order.
+            if x + y in board.keys():
+              squares.append(board[x + y])
+            else:
+              if is_white_square:
+                squares.append(WHITE_SQUARE)
+              else:
+                squares.append(BLACK_SQUARE)
+            is_white_square = not is_white_square
+        is_white_square = not is_white_square
+
+    print(BOARD_TEMPLATE.format(*squares))
 
 def print_help():
-  print('Interactive Chessboard')
-  print('by Al Sweigart al@inventwithpython.com')
-  print()
-  print('Pieces:')
-  print('  w - White, b - Black')
-  print('  P - Pawn, N - Knight, B - Bishop, R - Rook, Q - Queen, K - King')
-  print('Commands:')
-  print('  move e2 e4 - Moves the piece at e2 to e4')
-  print('  remove e2 - Removes the piece at e2')
-  print('  set e2 wP - Sets square e2 to a white pawn')
-  print('  reset - Resets pieces back to their starting squares')
-  print('  clear - Clears the entire board')
-  print('  fill wP - Fills entire board with white pawns.')
-  print('  quit - Quits the program')
+    print('Interactive Chess Board')
+    print('by Al Sweigart al@inventwithpython.com')
+    print()
+    print('Pieces:')
+    print('  w - White, b - Black')
+    print('  P - Pawn, N - Knight, B - Bishop, R - Rook, Q - Queen, K - King')
+    print('Commands:')
+    print('  move e2 e4 - Moves the piece at e2 to e4.')
+    print('  remove e2 - Removes the piece at e2.')
+    print('  set e2 wP - Sets square e2 to a white pawn.')
+    print('  reset - Reset pieces back to their starting squares.')
+    print('  clear - Clear the entire board.')
+    print('  fill wP - Fill entire board with white pawns.')
+    print('  help - Show this help information.')
+    print('  quit - Quits the program.')
+
 
 main_board = copy.copy(STARTING_PIECES)
 print_help()
-
 while True:
-  print_chessboard(main_board)
-  response = input('> ').split()
+    print_chess_board(main_board)
+    response = input('> ').split()
 
-  if response[0] == 'move':
-    main_board[response[2]] = main_board[response[1]]
-    del main_board[response[1]]
-  elif response[0] == 'remove':
-    del main_board[response[1]]
+    if response[0] == 'move':
+        main_board[response[2]] = main_board[response[1]]
+        del main_board[response[1]]
+    elif response[0] == 'remove':
+        del main_board[response[1]]
+    elif response[0] == 'set':
+        main_board[response[1]] = response[2]
+    elif response[0] == 'reset':
+        main_board = copy.copy(STARTING_PIECES)
+    elif response[0] == 'clear':
+        main_board = {}
+    elif response[0] == 'fill':
+        for y in '87654321':
+            for x in 'abcdefgh':
+                main_board[x + y] = response[1]
+    elif response[0] == 'help':
+        print_help()
+    elif response[0] == 'quit':
+        sys.exit()
