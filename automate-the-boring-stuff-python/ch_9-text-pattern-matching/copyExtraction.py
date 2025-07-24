8e58c6a3a3289209d7bf9f1123eab3368ebed370
requiem-of-zero <theesamwong@gmail.com>
2025-07-23 20:59:04 -0700

    Added regex skeleton

--- automate-the-boring-stuff-python/ch_9-text-pattern-matching/copyExtraction.py
+++ automate-the-boring-stuff-python/ch_9-text-pattern-matching/copyExtraction.py
@@ -10,3 +10,13 @@ Requirements:
 - Neatly format the matched strings into a single string to paste
 - Display some kind of message if no matches were found in the text
 '''
+
+import pyperclip, re
+
+phone_re = re.compile(r"(\d{3})-(\d{3})-(\d{4})")
+email_re = re.compile(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+
+emailSearch1 = re.search(email_re, "theesamwong@gmail.com")
+phoneSearch1 = re.search(phone_re, "123-415-2313")
+
+print(emailSearch1)
