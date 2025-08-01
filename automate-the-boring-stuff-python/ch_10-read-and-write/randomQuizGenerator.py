20a6a288f828a7bd37eddecc100b55e6cfcb1551
requiem-of-zero <theesamwong@gmail.com>
2025-07-31 20:43:03 -0700

    Added project 4 requirements

---
+++ automate-the-boring-stuff-python/ch_10-read-and-write/randomQuizGenerator.py
@@ -0,0 +1,18 @@
+'''
+We're a geography teacher with 35 students in our class and want to give a pop quiz
+on US state capitals. Unfortunately, our class has a few bad eggs in it, and you can't
+trust the students not to cheat. You'd like to randomize the order of questions so
+that each quiz is unique, making it impossible for anyone to crib answers from anyone else
+
+Requirements:
+
+* Creates 35 different quizzes
+* Creates 50 multiple-choice questions for each quiz, in random order
+* Provides the correct answer and three random wrong answers for each question, in random order
+* Writes the quizzes to 35 text files
+* Writes the answer keys to 35 text files
+* Store the states and their capitals in a dictionary
+* Call open(), write(), and close() for the quiz and answer key text files
+* Use random.shuffle() to randomize the order of the questions and multiple-choice options
+
+'''
