acdae075a206c93371897917897a67ff467d0794
requiem-of-zero <theesamwong@gmail.com>
2025-07-31 22:22:10 -0700

    Added ch_10 practice madlibs requirements

---
+++ automate-the-boring-stuff-python/ch_10-read-and-write/madLibs.py
@@ -0,0 +1,31 @@
+'''
+Requirements:
+
+Create Mad Libs program that reads in text files and lets the user add their
+own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file.
+
+Example text file:
+The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was
+unaffected by these events.
+
+The program would find these occurences and prompt the user to replace them
+
+Example output text file:
+The silly panda walked to the chandelier and then screamed. A nearby
+pickup truck was unaffected by these events.
+'''
+
+import re # For finding placeholder words
+from pathlib import Path # For file handling
+
+# TODO: Load input file
+
+
+# TODO: Define the regex pattern for placeholders
+
+
+# TODO: Replace each placeholder with user input
+
+# Go through each match and prompt
+
+# TODO: Write to a new file
