bca20a586b8581be3d85bf2025d55c5d5b5d490b
requiem-of-zero <theesamwong@gmail.com>
2025-08-02 17:16:28 -0700

    Completed working mad libs python automation with file I/O

--- automate-the-boring-stuff-python/ch_10-read-and-write/madLibs.py
+++ automate-the-boring-stuff-python/ch_10-read-and-write/madLibs.py
@@ -16,16 +16,37 @@ pickup truck was unaffected by these events.
 '''
 
 import re # For finding placeholder words
+import sys
 from pathlib import Path # For file handling
 
 # TODO: Load input file
+input_path = Path(sys.argv[1])
 
+if not input_path.exists():
+  print('Input file not found.')
+  exit()
+
+with input_path.open('r', encoding='UTF-8') as file:
+  content = file.read()
 
 # TODO: Define the regex pattern for placeholders
-
+madlibs_pattern = re.compile(r'\b(ADJECTIVE|NOUN|VERB|ADVERB)\b')
 
 # TODO: Replace each placeholder with user input
+def prompt_word(word_type):
+  article = 'an' if word_type[0] in 'AEIOU' else 'a'
+  return input(f'Enter {article} {word_type.lower()}: ')
 
 # Go through each match and prompt
+output = content
+for match in madlibs_pattern.findall(content):
+  replacement = prompt_word(match)
+  output = output.replace(match, replacement, 1)
 
 # TODO: Write to a new file
+output_path = Path(input_path).resolve().parent/Path('madlibs_output.txt')
+with output_path.open('w', encoding='UTF-8') as out_file:
+  out_file.write(output)
+
+print(f'\nMad Libs completed. Output saved to: {output_path}')
+print(output)
