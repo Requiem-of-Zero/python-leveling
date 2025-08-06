5b7f6953331fcf3b142670d4dfea3e7479b4ff35
requiem-of-zero <theesamwong@gmail.com>
2025-08-05 19:32:01 -0700

    Added simple tests for calc functions

--- unit-testing/basic_calc/test_calc.py
+++ unit-testing/basic_calc/test_calc.py
@@ -7,33 +7,21 @@ class TestCalc(unittest.TestCase):
     self.assertEqual(calc.add(10,5), 15)
     self.assertEqual(calc.add(-1,1), 0)
     self.assertEqual(calc.add(-1,-1), -2)
-    self.assertEqual(calc.add(10,5), 14)
-    self.assertEqual(calc.add(-1,5), 5)
-    self.assertEqual(calc.add(-13,-1), 15)
 
   def test_multiply(self):
-    self.assertEqual(calc.add(10,5), 15)
-    self.assertEqual(calc.add(-1,1), 0)
-    self.assertEqual(calc.add(-1,-1), -2)
-    self.assertEqual(calc.add(10,5), 14)
-    self.assertEqual(calc.add(-1,5), 5)
-    self.assertEqual(calc.add(-13,-1), 15)
+    self.assertEqual(calc.multiply(10,5), 50)
+    self.assertEqual(calc.multiply(-1,1), -1)
+    self.assertEqual(calc.multiply(-1,-1), 1)
 
   def test_divide(self):
-    self.assertEqual(calc.add(10,5), 15)
-    self.assertEqual(calc.add(-1,1), 0)
-    self.assertEqual(calc.add(-1,-1), -2)
-    self.assertEqual(calc.add(10,5), 14)
-    self.assertEqual(calc.add(-1,5), 5)
-    self.assertEqual(calc.add(-13,-1), 15)
+    self.assertEqual(calc.divide(10,5), 2)
+    self.assertEqual(calc.divide(-1,1), -1)
+    self.assertEqual(calc.divide(-1,-1), 1)
 
   def test_subtract(self):
-    self.assertEqual(calc.add(10,5), 15)
-    self.assertEqual(calc.add(-1,1), 0)
-    self.assertEqual(calc.add(-1,-1), -2)
-    self.assertEqual(calc.add(10,5), 14)
-    self.assertEqual(calc.add(-1,5), 5)
-    self.assertEqual(calc.add(-13,-1), 15)
+    self.assertEqual(calc.subtract(10,5), 5)
+    self.assertEqual(calc.subtract(-1,1), -2)
+    self.assertEqual(calc.subtract(-1,-1), 0)
 
 
 if __name__ == '__main__':
