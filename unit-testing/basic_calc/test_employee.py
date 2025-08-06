75603897f7bc426150c5c505017e226ff097140d
requiem-of-zero <theesamwong@gmail.com>
2025-08-05 21:08:23 -0700

    Added ways to specify portions of test lifecycle

--- unit-testing/basic_calc/test_employee.py
+++ unit-testing/basic_calc/test_employee.py
@@ -3,14 +3,24 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
-  def setUp(self):
+  @classmethod # working with class itself
+  def setUpClass(cls):
+    print('setupClass\n')
+
+  @classmethod
+  def tearDownClass(cls):
+    print('teardownClass')
+
+  def setUp(self): # Allows mock data variables you can use throughout unittest testcase class, working with instances
+    print('setUp')
     self.employee_1 = Employee('Sam', 'Wong', 90000)
     self.employee_2 = Employee('Mom', 'Dad', 95000)
 
-  def tearDown(self):
-    pass
+  def tearDown(self): # Allows you to delete any files created from this testcase after test is over
+    print('tearDown\n')
 
   def test_email(self):
+    print('testEmail')
     self.assertEqual(self.employee_1.email, 'Sam.Wong@email.com')
     self.assertEqual(self.employee_2.email, 'Mom.Dad@email.com')
 
@@ -21,6 +31,7 @@ class TestEmployee(unittest.TestCase):
     self.assertEqual(self.employee_2.email, 'Dad.Dad@email.com')
 
   def test_fullname(self):
+    print('testFullName')
     self.assertEqual(self.employee_1.fullname, 'Sam Wong')
     self.assertEqual(self.employee_2.fullname, 'Mom Dad')
 
@@ -31,6 +42,7 @@ class TestEmployee(unittest.TestCase):
     self.assertEqual(self.employee_2.fullname, 'Dad Dad')
      
   def test_apply_raise(self):
+    print('testApplyRaise')
     self.employee_1.apply_raise()
     self.employee_2.apply_raise()
 
