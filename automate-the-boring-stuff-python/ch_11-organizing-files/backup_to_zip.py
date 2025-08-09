26dccbd3a0bf5a625f2f934af5bf183811bfe9f7
requiem-of-zero <theesamwong@gmail.com>
2025-08-09 16:54:28 -0700

    Completed ch 11 project 5

--- automate-the-boring-stuff-python/ch_11-organizing-files/backup_to_zip.py
+++ automate-the-boring-stuff-python/ch_11-organizing-files/backup_to_zip.py
@@ -7,8 +7,33 @@ folder = Path(folder) Ensure the folder is a path object, not a string
 Figure out the ZIP filename this code should use, based on what files already exist
 '''
 
-import zipfile, os
+import zipfile, os, datetime
 from pathlib import Path
 
 def backup_to_zip(folder):
-  print(folder)
+  number = 1
+  while True:
+    zip_filename = Path(folder.parts[-1] + '_' + str(number) + '.zip')
+    print(zip_filename)
+    if not zip_filename.exists():
+      print(f'Found {zip_filename}')
+      break
+    number = number + 1
+
+  # TODO: Create the ZIP file
+  print(f'Creating {zip_filename}...')
+  backup_zip = zipfile.ZipFile(zip_filename, 'w')
+
+  # TODO: Walk the entire folder tree and compress the files in each folder.
+  for folder_name, subfolders, filenames in os.walk(folder):
+    folder_name = Path(folder_name)
+    print(f'Adding files in folder {folder_name}...')
+
+    # Add all the files in the folder to the ZIP file
+    for filename in filenames:
+      print(f'Adding file {filename}...')
+      backup_zip.write(folder_name/filename)
+  backup_zip.close()
+  print('Done.')
+
+backup_to_zip(Path.cwd()/'spam')
