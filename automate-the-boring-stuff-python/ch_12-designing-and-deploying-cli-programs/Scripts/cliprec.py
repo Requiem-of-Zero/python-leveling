5777682fb9355738651a543f06f34c0518484ffa
requiem-of-zero <theesamwong@gmail.com>
2025-08-10 12:48:53 -0700

    Adde ch_12 clipboard recorder requirements annotation

---
+++ automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/cliprec.py
@@ -0,0 +1,18 @@
+'''
+Requirements
+
+Automate these steps:
+- Right-click a link in a web browser
+- Select the Copy Link or Copy Link Address item from the context menu
+- Switch to the spreadsheet app
+- Press CTRL-V to paste the link
+- Switch back to the web browser
+
+Needs to be automated because it is tedious when a page has dozen or hundreds of links. 
+We can create a small clipboard-recording program to make it faster.
+The program will monitor the clipboard to see if new text has been copied to it, and if so, it will print it to the terminal screen
+
+We can convert our five-step process into two steps:
+- Right-click a link in a web browser
+- Select the Copy Link or Copy Link Address item from the context menu
+'''
