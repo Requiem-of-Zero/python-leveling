79bef65d71186e74411d9e8d1d345e3127f6fbc1
requiem-of-zero <theesamwong@gmail.com>
2025-08-10 13:14:40 -0700

    Added ch_12 working clipboard recorder with seen set

---
+++ automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/cliprec.command
@@ -0,0 +1,3 @@
+source ~/Documents/python-leveling/automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/.venv/bin/activate
+python3 ~/Documents/python-leveling/automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/cliprec.py
+deactivate
--- automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/cliprec.py
+++ automate-the-boring-stuff-python/ch_12-designing-and-deploying-cli-programs/Scripts/cliprec.py
@@ -16,3 +16,18 @@ We can convert our five-step process into two steps:
 - Right-click a link in a web browser
 - Select the Copy Link or Copy Link Address item from the context menu
 '''
+
+import pyperclip, time
+
+print('Recording clipboard... (Ctrl-C to stop)')
+seen = set()
+try:
+  while True:
+    content = pyperclip.paste() # Get clipboard content
+
+    if content not in seen: # If different from the previous, print it
+      print(content)
+      seen.add(content)
+    time.sleep(0.01) # Pause to avoid hogging CPU
+except KeyboardInterrupt:
+  pass
