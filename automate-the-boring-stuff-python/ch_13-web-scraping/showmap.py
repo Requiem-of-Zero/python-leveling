c324ef6dd78aa3435434a8424369f33ecacd8993
requiem-of-zero <theesamwong@gmail.com>
2025-08-10 13:54:38 -0700

    Added ch_13 web scraping project 6 requirements

---
+++ automate-the-boring-stuff-python/ch_13-web-scraping/showmap.py
@@ -0,0 +1,13 @@
+'''
+Requirements:
+
+- Get a street address from the command line arguments or clipboard
+- Opens the web browser to the OpenStreetMap page for that address
+
+We need to:
+Read the command line arguments from sys.argv
+Read the clipboard contents
+Call the webbrowser.open() to open the browser
+Open a new file editor tab and save it as showmap.py
+'''
+
