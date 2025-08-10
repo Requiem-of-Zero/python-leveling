2d4e1894b70e7ef4caae16a5f00b1a1f3adefc1e
requiem-of-zero <theesamwong@gmail.com>
2025-08-10 14:23:02 -0700

    Completed simple ch_13 automation to query an address from CLI and clipboard

--- automate-the-boring-stuff-python/ch_13-web-scraping/showmap.py
+++ automate-the-boring-stuff-python/ch_13-web-scraping/showmap.py
@@ -11,3 +11,13 @@ Call the webbrowser.open() to open the browser
 Open a new file editor tab and save it as showmap.py
 '''
 
+import webbrowser, sys, pyperclip
+if len(sys.argv) > 1:
+  # Get address from command line
+  address = " ".join(sys.argv[1:])
+else:
+# TODO: Get address from clipboard
+  address = pyperclip.paste()
+
+# TODO: Open the web browser
+webbrowser.open('https://www.openstreetmap.org/search?query='+address)
