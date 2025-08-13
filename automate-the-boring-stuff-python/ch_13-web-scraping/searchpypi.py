35b962609a57dde3ef86925dbae74a258e326ed1
requiem-of-zero <theesamwong@gmail.com>
2025-08-12 20:26:55 -0700

    Added ch_13 project 7 requirements

---
+++ automate-the-boring-stuff-python/ch_13-web-scraping/searchpypi.py
@@ -0,0 +1,11 @@
+'''
+Requirements
+
+- Get search keywords from the command line arguments
+  - Read the command line arguments from sys.argv
+- Retrieve the search results page
+  - Fetch the search results page with the requests module
+- Open a browser tab for each result
+  - Call the webbrowser.open() function to open the web browser
+'''
+
